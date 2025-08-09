0831bdd0f0b8fae670b97d5e7ffe6ba70e8985d5
Faithhacker03 <112615080+Faithhacker03@users.noreply.github.com>
2025-08-09 12:04:06 +0800

    Update routes.py

--- panel_app/routes.py
+++ panel_app/routes.py
@@ -2,18 +2,18 @@ import os
 from flask import render_template, url_for, flash, redirect, request, abort, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.utils import secure_filename
-# Import from our new extensions file
+# CORRECT: Import from our new extensions.py file
 from .extensions import db, bcrypt 
+# CORRECT: Import models and forms using relative paths
 from .models import User, Bot, BotStatus
 from .forms import RegistrationForm, LoginForm
 from config import Config
 
+# DEFINE the blueprint here.
 main = Blueprint('main', __name__)
 
-# --- ALL YOUR ROUTE FUNCTIONS GO HERE ---
-# (The content of your routes does not need to change, just the imports at the top)
-# (Copy all the @main.route functions from your existing file and paste them below this line)
-
+# --- Paste all your @main.route(...) functions below this line ---
+# --- The functions themselves do not need to change ---
 @main.route("/")
 @main.route("/home")
 def home():
@@ -30,29 +30,13 @@ def register():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(email=form.email.data, password=hashed_password)
         db.session.add(user)
+        # Every new user gets a Bot entry associated with them
         bot = Bot(owner=user, status=BotStatus.STOPPED)
         db.session.add(bot)
         db.session.commit()
         flash('Your account has been created! You are now able to log in.', 'success')
         return redirect(url_for('main.login'))
     return render_template('register.html', title='Register', form=form)
-
-# ... PASTE THE REST OF YOUR ROUTES: login, logout, dashboard, start_bot, stop_bot, admin_panel ...
-# The code you provided for routes.py in the previous message was correct from the function definitions downward.
-@main.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.dashboard'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.dashboard'))
-        else:
-            flash('Login Unsuccessful. Please check email and password.', 'danger')
-    return render_template('login.html', title='Login', form=form)
 
 @main.route("/logout")
 def logout():
